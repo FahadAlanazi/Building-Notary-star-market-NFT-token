238a8f0cac82deef10cbbaf03f0e45149e670bb5
FahadAlanazi <36148387+FahadAlanazi@users.noreply.github.com>
2019-01-23 19:41:32 +0300

    Delete secrets.js

--- secrets.js
+++
@@ -1,4 +0,0 @@
-var mnemonic = 'sustain thing foam reduce ghost powder still vehicle address harvest unit lazy';
-var infura = 'https://rinkeby.infura.io/v3/0dbe40b864be44bab617c523af4f42ff';
-
-module.exports(mnemonic, infura)
